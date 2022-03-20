58f89ca7476297d6955308e1a3c607a3234d6c94
Maksim Safronenko <maxnet.ops@gmail.com>
2022-03-21 01:46:55 +0300

    Добавлены urls и view-функции

---
+++ yatube/posts/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def group_posts(request, slug):
+    return HttpResponse('Посты, отфильтрованные по группам')
+
+# Create your views here.
