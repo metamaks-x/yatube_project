50b3af3696f0e73021f8d31bf741457659953f60
Maksim Safronenko <maxnet.ops@gmail.com>
2022-04-01 15:35:15 +0300

    полуфинал

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(verbose_name='URL')
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='group',
+        )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        )
--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('group/<slug:slug>/', views.group_posts),
-]
+    path('', views.index, name='index'),
+    path('index.html', views.index, name='index'),
+    path('group_list.html', views.group_posts, name='group_posts'),
+    path('group/<slug:slug>/', views.group_posts, name='group_posts'),
+    ]
