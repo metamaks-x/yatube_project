11bd5a53f798a3d8cbc3d4cfa35bbeef04749cee
Maksim Safronenko <maxnet.ops@gmail.com>
2022-04-03 03:07:47 +0300

    добавлены шаблоны и теги Django

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -5,7 +5,6 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('index.html', views.index, name='index'),
     path('group_list.html', views.group_posts, name='group_posts'),
     path('group/<slug:slug>/', views.group_posts, name='group_posts'),
     ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,20 +3,18 @@ from .models import Post, Group
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    template = 'posts/index.html'
     context = {
-        'posts': posts,
+        'title': 'Это главная страница Yatube',
     }
-    return render(request, 'posts/index.html', context)
+    return render(request, template, context)
 
 
-def group_posts(request, slug):
-    group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+def group_posts(request):
+    template = 'posts/group_list.html'
     context = {
-        'group': group,
-        'posts': posts,
+        'title': 'Здесь будет информация о группах проекта Yatube',
     }
-    return render(request, 'posts/group_list.html', context)
+    return render(request, template, context)
 
 # Create your views here.
